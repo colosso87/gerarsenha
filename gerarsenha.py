d1cd8f8bbc4cd72906b8decc7a9d5d99be7c917e
Thiago <thiagolealrocha@gmail.com>
2024-05-14 19:09:42 -0300

    Entrar com variavel

--- gerarsenha.py
+++ gerarsenha.py
@@ -1,1 +1,18 @@
-print ("Crie sua Senha")
+#maiúsculas e minúsculas
+#símbolos e espaços
+
+"""
+Security = chave
+5ecurt1ty = senha
+
+"""
+
+chave = input ("Digite a base da sua senha: ")
+
+senha = ""
+for letras in chave:
+    if letra in "Aa":
+        senha = senha + "10"
+    elif letras in "Bb":
+        senha = senha + "11"
+        
